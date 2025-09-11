5663bb54bfd8dff2a6ce81a15018d1f8f48a9635
Liam Nguyen <64422533+liam102@users.noreply.github.com>
2025-09-11 10:07:32 -0600

    add some more tools and coding languages

--- MacOS/extra.config.fish
+++ MacOS/extra.config.fish
@@ -5,3 +5,12 @@ set -x LDFLAGS -L/opt/homebrew/opt/openssl/lib
 
 set -U fish_user_paths /opt/homebrew/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/bin $fish_user_paths
 
+# Set the Go workspace
+set -gx GOPATH "$HOME/go"
+
+# For Homebrew on macOS
+# The `brew --prefix` command finds the correct installation path
+fish_add_path (brew --prefix go)/libexec/bin
+
+# Add binaries installed via `go install` to the PATH
+fish_add_path "$GOPATH/bin"
