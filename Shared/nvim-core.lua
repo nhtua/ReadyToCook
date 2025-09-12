56776d305d2fbb6860bca0da271e3fec059a7451
Liam Nguyen <5275627+nhtua@users.noreply.github.com>
2025-09-11 22:12:56 -0600

    update LazyVim configuration

--- Shared/nvim-core.lua
+++ Shared/nvim-core.lua
@@ -68,6 +68,12 @@ return {
         sorting_strategy = "ascending",
         winblend = 0,
       },
+      pickers = {
+        find_files = {
+          -- `hidden = true` will still show the inside of `.git/` as it's not `.gitignore`d.
+          find_command = { "rg", "--files", "--hidden", "--glob", "!**/.git/*" },
+        },
+      },
     },
   },
 
@@ -174,10 +180,10 @@ return {
           endpoint = "https://api.anthropic.com",
           model = "claude-sonnet-4-20250514",
           timeout = 30000, -- Timeout in milliseconds
-            extra_request_body = {
-              temperature = 0.75,
-              max_tokens = 20480,
-            },
+          extra_request_body = {
+            temperature = 0.75,
+            max_tokens = 20480,
+          },
         },
         gemini = {
           model = "gemini-2.5-flash",
@@ -187,7 +193,11 @@ return {
             max_tokens = 20480,
           },
         },
-        gpt = {
+        ["gemini-2.5-pro"]={
+          __inherited_from = "gemini",
+          model = "gemini-2.5-pro"
+        },
+        openai = {
           model = "gpt-5-mini",
           timeout = 30000,
           extra_request_body = {
